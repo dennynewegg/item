fc465c43acc312d3c6a0e6a39863ee8356f76ece
Denny yu <Denny.l.yu@newegg.com>
2016-09-01 17:52:52 +0800

    ok

---
+++ csharp/stock/stock/StockEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stock
+{
+    [Serializable]
+    public class StockEntity
+    {
+        public string StockCode { get; set; }
+        public string StockName { get; set; }
+        public DateTime? InDate { get; set; }
+        public decimal? Open { get; set; }
+        public decimal? Close { get; set; }
+        public decimal? High { get; set; }
+        public decimal? Low { get; set; }
+        public decimal? Volume { get; set; }
+        public decimal? Percent { get; set; }
+        public decimal? Turnover { get; set; }
+
+        public decimal? AvgCost
+        {
+            get
+            {
+                if (Turnover.GetValueOrDefault() > 0
+                    && Volume.GetValueOrDefault() > 0)
+                {
+                    return Math.Round(Turnover.Value/Volume.Value, 2);
+                }
+                if (Close.HasValue)
+                {
+                    return Close;
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ csharp/stock/stock/WYStockBiz.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newegg.API.Client;
+
+namespace stock
+{
+    public static class WYStockBiz
+    {
+        private const string hq163url =
+            "http://quotes.money.163.com/hs/service/diyrank.php?page=0&query=STYPE:EQA&sort=VOLUME&order=desc&count={0}&type=query";
+
+        public static List<StockEntity> GetTradeList(int count = 3500)
+        {
+            var client = new RestAPIClient(string.Format(hq163url,count)) {Proxy = WebProxy.GetDefaultProxy()};
+            if (client.Proxy != null)
+            {
+                client.Proxy.Credentials = CredentialCache.DefaultNetworkCredentials;
+            }
+           
+            var result = client.Get<hq>(string.Empty, null);
+            if (result != null
+                && result.list != null)
+            {
+                return result.list.Select(item =>
+                    new StockEntity
+                    {
+                        StockCode = item.SYMBOL,
+                        Open = item.OPEN,
+                        Low = item.LOW,
+                        High = item.HIGH,
+                        Percent = item.PERCENT,
+                        Close = item.PRICE,
+                        StockName = item.SNAME,
+                        InDate = DateTime.Parse(item.TIME),
+                        Volume = item.VOLUME,
+                        Turnover = item.TURNOVER
+                    }
+                ).ToList();
+            }
+            return new List<StockEntity>();
+        }
+
+
+        class hq
+        {
+            public List<hqitem> list { get; set; }
+        }
+
+        private class hqitem
+        {
+            public decimal? HIGH { get; set; }
+            public decimal? LOW { get; set; }
+            public decimal? OPEN { get; set; }
+            public decimal? PERCENT { get; set; }
+            public decimal? PRICE { get; set; }
+            public string SNAME { get; set; }
+            public string SYMBOL { get; set; }
+            public string TIME { get; set; }
+            public decimal? VOLUME { get; set; }
+            public decimal? TURNOVER { get; set; }
+        }
+
+    }
+}
